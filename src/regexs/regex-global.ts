ecdf08d190157e308c96d13786b7c7e1cd02afcc
yoones <yooneszaamni58@gmail.com>
2025-08-20 17:04:50 +0330

    add message

--- src/regexs/regex-global.ts
+++ src/regexs/regex-global.ts
@@ -1,4 +1,4 @@
-const phoneRegex = new RegExp(/^09\d{9}$/g);
+const phoneRegex = /^(0?9\d{9}|98\d{9}|\d{6,15})$/;
 const passwordRegex = new RegExp(
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g,
 );
