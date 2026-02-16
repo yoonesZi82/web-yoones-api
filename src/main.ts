171853bd334514a0f701912388a8ae675912b581
yoones <yooneszaamni58@gmail.com>
2026-02-16 23:11:41 +0330

    debug

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
+  // app.enableCors({
+  //   origin: ['https://www.yoones-web.ir'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
+
   app.enableCors({
-    origin: ['https://www.yoones-web.ir'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: true,
     credentials: true,
   });
 
@@ -20,13 +25,12 @@ async function bootstrap() {
     .setTitle('Web Yoones Api')
     .setDescription('Description for web yoones api')
     .setVersion('1.0.0')
-    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
   console.log(`Server is running on port ${process.env.PORT ?? 3001}`);
 }
 bootstrap();
